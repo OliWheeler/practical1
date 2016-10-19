406fabdfd71a0b921d235ed8ca070399559d7d82
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-10-19 15:21:57 +0100

    python script subplots, error, labels

--- graph_gen.py
+++ graph_gen.py
@@ -17,15 +17,31 @@ with open(filename, 'r') as f:
             cols = [float(l) for l in row]
             df_list.append(cols)
 
-times = map(lambda x: x[0], df_list)
+timestamps = map(lambda x: x[0], df_list)
 ref_angle_1 = map(lambda x: abs(x[1]), df_list)
 angle_1 = map(lambda x: abs(x[2]), df_list)
+error_1 = [(ang - ref) for (ang, ref) in zip(angle_1, ref_angle_1)]
 ref_angle_2 = map(lambda x: abs(x[3]), df_list)
 angle_2 = map(lambda x: abs(x[4]), df_list)
+error_2 = [(ang - ref) for (ang, ref) in zip(angle_2, ref_angle_2)]
 
-plt.plot(times, ref_angle_1)
-plt.plot(times, angle_1)
-plt.plot(times, ref_angle_2)
-plt.plot(times, angle_2)
+plt.subplot(1, 2, 1)
+plt.plot(timestamps, ref_angle_1, label="reference angle 1")
+plt.plot(timestamps, angle_1, label="angle 1")
+plt.plot(timestamps, error_1, label="error")
+plt.xlabel('time (s)')
+plt.ylabel('angle (radians)')
+plt.legend(loc=0)
+plt.title('Motor 1')
 
+plt.subplot(1, 2, 2)
+plt.plot(timestamps, ref_angle_2, label="reference angle 2")
+plt.plot(timestamps, angle_2, label="angle 2")
+plt.plot(timestamps, error_2, label="error")
+plt.xlabel('time (s)')
+plt.ylabel('angle (radians)')
+plt.legend(loc=0)
+plt.title('Motor 2')
+
+plt.suptitle('Angle values before tuning')
 plt.show()
