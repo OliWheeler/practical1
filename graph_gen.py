c1bc1635449f5c09784db9a43bcc1b6c5fd6f340
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-10-19 17:39:26 +0100

    grid in graphs, logfiles for untuned and minPWM tuned motors

--- graph_gen.py
+++ graph_gen.py
@@ -17,13 +17,15 @@ with open(filename, 'r') as f:
             cols = [float(l) for l in row]
             df_list.append(cols)
 
-timestamps = map(lambda x: x[0], df_list)
+timestamps  = map(lambda x: x[0], df_list)
+
 ref_angle_1 = map(lambda x: abs(x[1]), df_list)
-angle_1 = map(lambda x: abs(x[2]), df_list)
-error_1 = [(ang - ref) for (ang, ref) in zip(angle_1, ref_angle_1)]
+angle_1     = map(lambda x: abs(x[2]), df_list)
+error_1     = [(ang - ref) for (ang, ref) in zip(angle_1, ref_angle_1)]
+
 ref_angle_2 = map(lambda x: abs(x[3]), df_list)
-angle_2 = map(lambda x: abs(x[4]), df_list)
-error_2 = [(ang - ref) for (ang, ref) in zip(angle_2, ref_angle_2)]
+angle_2     = map(lambda x: abs(x[4]), df_list)
+error_2     = [(ang - ref) for (ang, ref) in zip(angle_2, ref_angle_2)]
 
 plt.subplot(1, 2, 1)
 plt.plot(timestamps, ref_angle_1, label="reference angle 1")
@@ -32,6 +34,8 @@ plt.plot(timestamps, error_1, label="error")
 plt.xlabel('time (s)')
 plt.ylabel('angle (radians)')
 plt.legend(loc=0)
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
 plt.title('Motor 1')
 
 plt.subplot(1, 2, 2)
@@ -41,6 +45,8 @@ plt.plot(timestamps, error_2, label="error")
 plt.xlabel('time (s)')
 plt.ylabel('angle (radians)')
 plt.legend(loc=0)
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
 plt.title('Motor 2')
 
 plt.suptitle('Angle values before tuning')
