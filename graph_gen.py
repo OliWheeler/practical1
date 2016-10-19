20113dbb90f9b89fa99c2ef015fef597496ce75b
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-10-19 14:45:16 +0100

    plotting with filename arg

--- graph_gen.py
+++ graph_gen.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-
+import sys
 
 df_list = []
 
-with open('logfile.txt', 'r') as f:
+filename = 'logfile.txt'
+
+if len(sys.argv) >= 2:
+    filename = sys.argv[1]
+
+with open(filename, 'r') as f:
     for line in f:
         # remove whitespace at the start and the newline at the end
         line = line.strip()
         row = line.split("\t")
-        if len(row) != 1:
+        if len(row) == 5:  # skip lines with only one value
             cols = [float(l) for l in row]
             df_list.append(cols)
-
-
 
 times = map(lambda x: x[0], df_list)
 ref_angle_1 = map(lambda x: abs(x[1]), df_list)
@@ -26,12 +28,4 @@ plt.plot(times, angle_1)
 plt.plot(times, ref_angle_2)
 plt.plot(times, angle_2)
 
-#, angle_1, ref_angle_2, angle_2)
-
-
 plt.show()
-savefig("graph.png")
-#df = pd.DataFrame(df_list)
-#print(df)
-
-#df.plot()
